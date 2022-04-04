44ddcaffb1ce55f7918a822d0728bfc3159d0b9d
jacu9585 <jacu9585@colorado.edu>
2022-04-04 15:38:04 -0600

    Adding gulp test file

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,24 @@
+const sass = require('gulp-sass');
+
+function generateCSS(cb) {
+    src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('public/stylesheets'));
+    cb();
+}
+
+exports.css = generateCSS;
+
+
+function copy(cb) {
+    src('routes/*.js')
+        .pipe(dest('copies'));
+    cb();
+}
+
+exports.copy = copy;
+
+
 function mytask(callback) {
    // task body
    callback();
